8293da640d93336b42f26b4b1fc6c549ca2a1543
Educpf <eduardocportugalf@gmail.com>
2023-12-07 11:49:42 +0000

    Made ImageFactory class follow the Singleton Pattern

--- app/src/main/java/com/ldts/asphaltrush/model/ImageFactory.java
+++ app/src/main/java/com/ldts/asphaltrush/model/ImageFactory.java
@@ -2,7 +2,16 @@ package com.ldts.asphaltrush.model;
 
 public class ImageFactory {
 
+    private static ImageFactory instance;
 
 
+
+
+    public static ImageFactory getInstance(){
+        if (instance == null){
+            instance = new ImageFactory();
+        }
+        return instance;
+    }
 
 }
