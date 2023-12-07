c051722f687543a7aa3f2874d202e6c27f1554c9
Educpf <eduardocportugalf@gmail.com>
2023-12-07 12:40:49 +0000

    Implemented ImageFactory Methods

--- app/src/main/java/com/ldts/asphaltrush/model/ImageFactory.java
+++ app/src/main/java/com/ldts/asphaltrush/model/ImageFactory.java
@@ -1,7 +1,9 @@
 package com.ldts.asphaltrush.model;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 
 public class ImageFactory {
@@ -13,14 +15,38 @@ public class ImageFactory {
     }
 
     public Image getImage(String image){
-
+        if (!images.containsKey(image)) {
+            Image loadedImage = loadImageFromFile(image);
+            images.put(image, loadedImage);
+        }
+        return images.get(image);
     }
 
     private Image loadImageFromFile(String image){
+        URL resource = ImageFactory.class.getResource(image);
+        assert resource != null;
+        try {
 
+            BufferedReader br = new BufferedReader(new FileReader(resource.getFile()));
+            return readImage(br);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    private Image readImage(BufferedReader bufferReader){
+    private Image readImage(BufferedReader br) throws IOException {
+        int width = Integer.parseInt(br.readLine());
+        int height = Integer.parseInt(br.readLine());
+
+        String[][] colorMatrix = new String[height][width];
+
+        for (int i=0; i<height; i++){
+            String line = br.readLine();
+            colorMatrix[i] = line.split("-");
+        }
+
+        return new Image(width, height, colorMatrix);
 
     }
 
