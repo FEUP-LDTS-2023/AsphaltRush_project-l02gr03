26f90a34fa29cdf8122e1961bdddda9149ede15b
Educpf <eduardocportugalf@gmail.com>
2023-12-07 11:55:27 +0000

    Declared the methods responsable for retriving an Image in ImageFactory Class, making it follow the Flyweight pattern

--- app/src/main/java/com/ldts/asphaltrush/model/ImageFactory.java
+++ app/src/main/java/com/ldts/asphaltrush/model/ImageFactory.java
@@ -1,10 +1,28 @@
 package com.ldts.asphaltrush.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class ImageFactory {
 
     private static ImageFactory instance;
+    private HashMap<String, Image> images;
+    public ImageFactory(){
+        images =  new HashMap<>();
+    }
 
+    public Image getImage(String image){
 
+    }
+
+    private Image loadImageFromFile(String image){
+
+    }
+
+    private Image readImage(BufferedReader bufferReader){
+
+    }
 
 
     public static ImageFactory getInstance(){
