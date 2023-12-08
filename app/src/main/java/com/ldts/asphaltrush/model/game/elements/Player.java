c3a4a7e2d884a03bb61b66c195946ed80694827c
brunomgferreira <brunomgferreira17@gmail.com>
2023-12-08 10:11:39 +0000

    Colisions with power-ups are checked

--- app/src/main/java/com/ldts/asphaltrush/model/game/elements/Player.java
+++ app/src/main/java/com/ldts/asphaltrush/model/game/elements/Player.java
@@ -1,11 +1,15 @@
 package com.ldts.asphaltrush.model.game.elements;
 
+import com.ldts.asphaltrush.model.game.elements.powerup.PowerUp;
+
 public class Player extends Element {
 
     private double speed = 1;
     private double minSpeed = 1;
     private double maxSpeed = 3.4;
     private boolean crashed = false;
+    private PowerUp powerUp;
+    private double powerUpTime;
 
     public Player(int x, int y) {
         super(x, y);
@@ -23,11 +27,38 @@ public class Player extends Element {
         speed = Math.max(minSpeed, speed-0.4);
     }
 
+    public void increaseMinSpeed() {
+        minSpeed += 0.0005;
+        speed = Math.max(speed, minSpeed);
+    }
+
     public boolean getCrashed() {
         return this.crashed;
     }
 
     public void setCrashed() {
         this.crashed = true;
+    }
+
+    public PowerUp getPowerUp() {
+        return this.powerUp;
+    }
+
+    public void addPowerUp(PowerUp powerUp) {
+        this.powerUp = powerUp;
+        this.powerUpTime = 15.0;
+    }
+
+    public void removePowerUp() {
+        this.powerUp = null;
+        powerUpTime = 0;
+    }
+
+    public double getPowerUpTime() {
+        return this.powerUpTime;
+    }
+
+    public void decreasePowerUpTime() {
+        powerUpTime-=0.1;
     }
 }
