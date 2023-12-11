1712d5168943f78a7e344dc489b742ecba8c6303
Xavier Martins <xavidosantos@gmail.com>
2023-12-11 20:55:12 +0000

    Created Observer and adapt GameState in order to use observer pattern

--- app/src/main/java/com/ldts/asphaltrush/states/GameState.java
+++ app/src/main/java/com/ldts/asphaltrush/states/GameState.java
@@ -1,13 +1,23 @@
 package com.ldts.asphaltrush.states;
 
+import com.ldts.asphaltrush.Game;
+import com.ldts.asphaltrush.Observer.GameStateObserver;
 import com.ldts.asphaltrush.model.menu.Menu;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GameState {
 
     private State state;
+    private List<GameStateObserver> observers;
     public GameState(){
         this.state = new MenuState(new Menu());
+        this.observers = new ArrayList<>();
     }
 
     public void setState(State state) {
@@ -16,5 +26,16 @@ public class GameState {
     public State getState() {
         return state;
     }
+
+    public void addObserver(GameStateObserver observer) {
+        observers.add(observer);
+    }
+
+    private void notifyObservers() throws IOException, LineUnavailableException, UnsupportedAudioFileException{
+        for (GameStateObserver observer : observers) {
+            observer.update();
+        }
+    }
+
 }
 
