0cc5f2fdde667f08bba5c0278097f19175222f38
Educpf <eduardocportugalf@gmail.com>
2023-12-11 10:57:54 +0000

    Fixed path to font

--- app/src/main/java/com/ldts/asphaltrush/gui/LanternaGUI.java
+++ app/src/main/java/com/ldts/asphaltrush/gui/LanternaGUI.java
@@ -47,7 +47,7 @@ public class LanternaGUI implements GUI {
 
 
     private AWTTerminalFontConfiguration loadSquareFont() throws URISyntaxException, FontFormatException, IOException {
-        URL resource = getClass().getClassLoader().getResource("fonts/square5.ttf");
+        URL resource = getClass().getClassLoader().getResource("fonts/square.ttf");
         assert resource != null;
         File fontFile = new File(resource.toURI());
         Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
