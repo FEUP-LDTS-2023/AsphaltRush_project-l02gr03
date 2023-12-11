cfbf5b693567241f10749326d44bffcae1a81978
Educpf <eduardocportugalf@gmail.com>
2023-12-11 18:14:36 +0000

    Polished Menu Viewer and updated the name of the resources associated with it
    Co-authored by: Xptm15 <xavidosantos@gmail.com>

--- app/src/main/java/com/ldts/asphaltrush/viewer/menu/MenuViewer.java
+++ app/src/main/java/com/ldts/asphaltrush/viewer/menu/MenuViewer.java
@@ -1,6 +1,7 @@
 package com.ldts.asphaltrush.viewer.menu;
 
 import com.ldts.asphaltrush.gui.GUI;
+import com.ldts.asphaltrush.model.Image;
 import com.ldts.asphaltrush.model.ImageFactory;
 import com.ldts.asphaltrush.model.Position;
 import com.ldts.asphaltrush.model.menu.Menu;
@@ -13,14 +14,17 @@ public class MenuViewer extends Viewer<Menu> {
 
     @Override
     protected void drawElements(GUI gui, ImageFactory imageFactory) {
-        gui.drawText(new Position(15, 15), "Menu", "#FFFFFF");
+        gui.drawImage(new Position(0, 0), imageFactory.getImage("/background/menu.txt"));
+        gui.drawImage(new Position(40, 15), imageFactory.getImage("/titles/menu/MainTitle.txt"));
 
-        for (int i = 0; i < getModel().getNumberEntries(); i++)
-            gui.drawText(
-                    new Position(15, 17 + i),
-                    getModel().getEntry(i),
-                    getModel().isSelected(i) ? "#0096FF" : "#FFFFFF");
-        gui.drawText(new Position(15, 25), getModel().getCurrentCar(), "#FFFFFF");
+        for (int i = 0; i < getModel().getNumberEntries(); i++){
+
+            String extra = getModel().isSelected(i) ? "2" : "";
+
+            Image entryImage = imageFactory.getImage("/titles/menu/" + getModel().getEntry(i) +  extra +   ".txt");
+            gui.drawImage(new Position(130 - entryImage.getWidth()/2, 100 + i * 20), entryImage);
+
+        }
 
     }
 }
