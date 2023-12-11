0aae0d9b2fcb0b493740c99554b0f5fa9119f708
Oferecido <ricardo.parreira.tic@gmail.com>
2023-12-11 12:39:38 +0000

    Ranking is working

--- app/src/main/java/com/ldts/asphaltrush/viewer/ranking/RankingViewer.java
+++ app/src/main/java/com/ldts/asphaltrush/viewer/ranking/RankingViewer.java
@@ -1,8 +1,12 @@
 package com.ldts.asphaltrush.viewer.ranking;
 
 import com.ldts.asphaltrush.gui.GUI;
+import com.ldts.asphaltrush.model.Position;
 import com.ldts.asphaltrush.model.ranking.Ranking;
 import com.ldts.asphaltrush.viewer.Viewer;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class RankingViewer extends Viewer<Ranking> {
     public RankingViewer(Ranking model) {
@@ -10,7 +14,23 @@ public class RankingViewer extends Viewer<Ranking> {
     }
 
     @Override
-    protected void drawElements(GUI gui) {
+    protected void drawElements(GUI gui) throws IOException {
+        gui.drawText(new Position(5, 5), "Ranking", "#FFFFFF");
 
+        ArrayList<String> rankingArray = getModel().getRankingAsText();
+        if(rankingArray != null) {
+            for (int i = 0; i < rankingArray.size(); i++)
+                gui.drawText(new Position(5, 7 + i), rankingArray.get(i), "#FFFFFF");
+        }
+        else {
+            gui.drawText(new Position(5, 10), "No scores", "#FFFFFF");
+            gui.drawText(new Position(5, 11), "Go play!", "#FFFFFF");
+        }
+
+        for (int i = 0; i < getModel().getNumberEntries(); i++)
+            gui.drawText(
+                    new Position(5, 20 + i),
+                    getModel().getEntry(i),
+                    getModel().isSelected(i) ? "#FFD700" : "#FFFFFF");
     }
 }
