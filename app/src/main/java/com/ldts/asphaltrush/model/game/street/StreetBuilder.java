87d149bf46e06677ec0a515defef0c965ce67e60
Educpf <eduardocportugalf@gmail.com>
2023-12-12 17:27:19 +0000

    Changed the behavior of the StreetBuilder Class

--- app/src/main/java/com/ldts/asphaltrush/model/game/street/StreetBuilder.java
+++ app/src/main/java/com/ldts/asphaltrush/model/game/street/StreetBuilder.java
@@ -1,103 +1,53 @@
 package com.ldts.asphaltrush.model.game.street;
 
+import com.ldts.asphaltrush.model.Image;
+import com.ldts.asphaltrush.model.ImageFactory;
 import com.ldts.asphaltrush.model.game.elements.*;
 import com.ldts.asphaltrush.model.game.elements.powerup.InvenciblePowerUp;
 import com.ldts.asphaltrush.model.game.elements.powerup.PointMultiplierPowerUp;
 import com.ldts.asphaltrush.model.game.elements.powerup.PowerUp;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class StreetBuilder {
 
-    private final int width;
-    private final int height;
 
-    public StreetBuilder(int width, int height) {
-        this.width = width;
-        this.height = height;
+    private String playerType;
+    private ImageFactory imageFactory;
+
+    public StreetBuilder(String playerType, ImageFactory imageFactory ){
+        this.playerType = playerType;
+        this.imageFactory = imageFactory;
     }
 
     public Street createStreet() {
-        Street street = new Street(width, height);
+        Street street = new Street();
 
         street.setPlayer(createPlayer());
-        street.setBarriers(createBarriers());
         street.setLines(createLines());
-        street.setObstacleCars(createObstacleCars());
-        street.setHoles(createHoles());
-        street.setJumps(createJumps());
-        street.setPowerUps(createPowerUps());
+        street.setBarriers(new ArrayList<Barrier>());
+        street.setHoles(new ArrayList<>());
+        street.setJumps(new ArrayList<>());
+        street.setObstacleCars(new ArrayList<>());
+        street.setPowerUps(new ArrayList<>());
         return street;
     }
 
-    private List<Barrier> createBarriers() {
-        List<Barrier> barriers = new ArrayList<>();
-
-        for (int y = 0; y < height; y++) {
-            barriers.add(new Barrier(0, y));
-            barriers.add(new Barrier(width - 1, y));
-        }
-
-        return barriers;
-    }
 
     private Player createPlayer() {
-        return new Player(width / 2, height - 2);
+        Image player = imageFactory.getImage("/cars/player/car" + playerType);
+        return new Player(190,235-player.getHeight() ,player.getWidth(), player.getHeight());
     }
 
-    private List<Line> createLines() {
-        List<Line> lines = new ArrayList<>();
+    private LinkedList<Line> createLines() {
+        LinkedList<Line> lines = new LinkedList<>();
 
-        for (int y = -3; y < height; y+=3) {
-            lines.add(new Line(4, y));
-            lines.add(new Line(8, y));
-            lines.add(new Line(12, y));
-            lines.add(new Line(16, y));
+        for (int y=0; y<240 ; y+=33){
+            lines.addLast(new Line(87, y));
         }
-
         return lines;
     }
 
-    private List<ObstacleCar> createObstacleCars() {
-        List<ObstacleCar> obstacleCars = new ArrayList<>();
-
-        obstacleCars.add(new ObstacleCar(2, 5));
-        obstacleCars.add(new ObstacleCar(6, 8));
-        obstacleCars.add(new ObstacleCar(10, 20));
-        obstacleCars.add(new ObstacleCar(14, 15));
-
-        return obstacleCars;
-    }
-
-    private List<Hole> createHoles() {
-        List<Hole> holes = new ArrayList<>();
-
-        holes.add(new Hole(2, 30));
-        holes.add(new Hole(6, 16));
-        holes.add(new Hole(10, 20));
-        holes.add(new Hole(14, 10));
-
-        return holes;
-    }
-
-    private List<Jump> createJumps() {
-        List<Jump> jumps = new ArrayList<>();
-
-        jumps.add(new Jump(2, 10));
-        jumps.add(new Jump(6, 20));
-        jumps.add(new Jump(10, 16));
-        jumps.add(new Jump(14, 30));
-
-        return jumps;
-    }
-
-    private List<PowerUp> createPowerUps() {
-        List<PowerUp> powerUps = new ArrayList<>();
-
-        powerUps.add(new InvenciblePowerUp(2, 14));
-        powerUps.add(new PointMultiplierPowerUp(18, 20));
-
-        return powerUps;
-    }
 }
