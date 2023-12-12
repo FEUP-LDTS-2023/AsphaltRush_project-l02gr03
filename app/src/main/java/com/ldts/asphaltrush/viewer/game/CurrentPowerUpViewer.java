49d5055d150eb0b5f34857fd8e6bf162823cbce2
Educpf <eduardocportugalf@gmail.com>
2023-12-12 18:01:08 +0000

    Updated PowerUp viewers

--- app/src/main/java/com/ldts/asphaltrush/viewer/game/CurrentPowerUpViewer.java
+++ app/src/main/java/com/ldts/asphaltrush/viewer/game/CurrentPowerUpViewer.java
@@ -2,6 +2,7 @@ package com.ldts.asphaltrush.viewer.game;
 
 import com.ldts.asphaltrush.gui.GUI;
 import com.ldts.asphaltrush.model.ImageFactory;
+import com.ldts.asphaltrush.model.Position;
 import com.ldts.asphaltrush.model.game.elements.powerup.InvenciblePowerUp;
 import com.ldts.asphaltrush.model.game.elements.powerup.PointMultiplierPowerUp;
 import com.ldts.asphaltrush.model.game.elements.powerup.PowerUp;
@@ -11,21 +12,8 @@ import java.util.Map;
 
 public class CurrentPowerUpViewer implements ElementViewer<PowerUp> {
 
-    private Map<Class<? extends PowerUp>, PowerUpDrawFunction> drawFunctions;
-
-    public CurrentPowerUpViewer() {
-        this.drawFunctions = new HashMap<>();
-        drawFunctions.put(PointMultiplierPowerUp.class, (powerUp, gui) -> gui.drawPointMultiplierPowerUp(powerUp.getPosition()));
-        drawFunctions.put(InvenciblePowerUp.class, (powerUp, gui) -> gui.drawInvenciblePowerUp(powerUp.getPosition()));
-    }
-
     @Override
     public void draw(PowerUp powerUp, GUI gui, ImageFactory imageFactory) {
-        PowerUpDrawFunction drawFunction = drawFunctions.getOrDefault(powerUp.getClass(), drawFunctions.get(PowerUp.class));
-        drawFunction.draw(powerUp, gui);
-    }
-
-    public interface PowerUpDrawFunction {
-        void draw(PowerUp powerUp, GUI gui);
+        gui.drawImage(new Position(230, 200), imageFactory.getImage("/elements/"+powerUp.getType()));
     }
 }
--- app/src/main/java/com/ldts/asphaltrush/viewer/game/PowerUpViewer.java
+++ app/src/main/java/com/ldts/asphaltrush/viewer/game/PowerUpViewer.java
@@ -1,6 +1,7 @@
 package com.ldts.asphaltrush.viewer.game;
 
 import com.ldts.asphaltrush.gui.GUI;
+import com.ldts.asphaltrush.model.Image;
 import com.ldts.asphaltrush.model.ImageFactory;
 import com.ldts.asphaltrush.model.game.elements.powerup.InvenciblePowerUp;
 import com.ldts.asphaltrush.model.game.elements.powerup.PointMultiplierPowerUp;
@@ -11,21 +12,9 @@ import java.util.Map;
 
 public class PowerUpViewer implements ElementViewer<PowerUp> {
 
-    private Map<Class<? extends PowerUp>, PowerUpDrawFunction> drawFunctions;
-
-    public PowerUpViewer() {
-        this.drawFunctions = new HashMap<>();
-        drawFunctions.put(PointMultiplierPowerUp.class, (powerUp, gui) -> gui.drawPointMultiplierPowerUp(powerUp.getPosition()));
-        drawFunctions.put(InvenciblePowerUp.class, (powerUp, gui) -> gui.drawInvenciblePowerUp(powerUp.getPosition()));
-    }
-
-    public interface PowerUpDrawFunction {
-        void draw(PowerUp powerUp, GUI gui);
-    }
-
     @Override
     public void draw(PowerUp element, GUI gui, ImageFactory imageFactory) {
-        PowerUpDrawFunction drawFunction = drawFunctions.getOrDefault(element.getClass(), drawFunctions.get(PowerUp.class));
-        drawFunction.draw(element, gui);
+        Image image = imageFactory.getImage("/elements/"+element.getType());
+        gui.drawImage(element.getPosition(), image);
     }
 }
