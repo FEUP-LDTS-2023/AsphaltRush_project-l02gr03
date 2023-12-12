d0d603615df76c2748124bdf45beda7f34094101
Educpf <eduardocportugalf@gmail.com>
2023-12-12 18:03:31 +0000

    Added a type in the PowerUp class in order to distinguish between Powerups

--- app/src/main/java/com/ldts/asphaltrush/model/game/elements/powerup/PointMultiplierPowerUp.java
+++ app/src/main/java/com/ldts/asphaltrush/model/game/elements/powerup/PointMultiplierPowerUp.java
@@ -2,6 +2,7 @@ package com.ldts.asphaltrush.model.game.elements.powerup;
 
 public class PointMultiplierPowerUp extends PowerUp {
     public PointMultiplierPowerUp(int x, int y) {
-        super(x,y);
+        super(x,y, "pointMultiplier");
     }
+
 }
--- app/src/main/java/com/ldts/asphaltrush/model/game/elements/powerup/PowerUp.java
+++ app/src/main/java/com/ldts/asphaltrush/model/game/elements/powerup/PowerUp.java
@@ -3,7 +3,14 @@ package com.ldts.asphaltrush.model.game.elements.powerup;
 import com.ldts.asphaltrush.model.game.elements.Element;
 
 public class PowerUp extends Element {
-    public PowerUp(int x, int y) {
+
+    private String type;
+
+    public String getType(){
+        return type;
+    }
+    public PowerUp(int x, int y, String type) {
         super(x, y);
+        this.type = type;
     }
 }
