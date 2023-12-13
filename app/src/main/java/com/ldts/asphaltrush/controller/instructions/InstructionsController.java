b5fe0ea93bb842ad78e4e2f14b7ff439a5fe8c8c
Xavier Martins <xavidosantos@gmail.com>
2023-12-13 04:02:29 +0000

    Implemented InstructionsController constructor and methods

--- app/src/main/java/com/ldts/asphaltrush/controller/instructions/InstructionsController.java
+++ app/src/main/java/com/ldts/asphaltrush/controller/instructions/InstructionsController.java
@@ -1,4 +1,31 @@
 package com.ldts.asphaltrush.controller.instructions;
 
-public class InstructionsController {
+import com.ldts.asphaltrush.Game;
+import com.ldts.asphaltrush.controller.Controller;
+import com.ldts.asphaltrush.gui.GUI;
+import com.ldts.asphaltrush.model.instructions.Instructions;
+import com.ldts.asphaltrush.model.menu.Menu;
+import com.ldts.asphaltrush.model.soundEffects.SelectSound;
+import com.ldts.asphaltrush.model.soundEffects.SoundEffect;
+import com.ldts.asphaltrush.states.MenuState;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class InstructionsController extends Controller<Instructions> {
+    private final SoundEffect selectSound;
+    public InstructionsController(Instructions model) {
+        super(model);
+        this.selectSound = new SelectSound();
+    }
+
+    @Override
+    public void step(Game game, GUI.ACTION action, long time) throws IOException, URISyntaxException, FontFormatException {
+        switch (action) {
+            case SELECT:
+                selectSound.play();
+                if (getModel().isSelectedBack()) game.getGameState().setState(new MenuState(new Menu()));
+        }
+    }
 }
--- app/src/main/java/com/ldts/asphaltrush/viewer/instructions/InstructionsViewer.java
+++ app/src/main/java/com/ldts/asphaltrush/viewer/instructions/InstructionsViewer.java
@@ -1,4 +1,19 @@
 package com.ldts.asphaltrush.viewer.instructions;
 
-public class InstructionsViewer {
+import com.ldts.asphaltrush.gui.GUI;
+import com.ldts.asphaltrush.model.ImageFactory;
+import com.ldts.asphaltrush.model.instructions.Instructions;
+import com.ldts.asphaltrush.viewer.Viewer;
+
+import java.io.IOException;
+
+public class InstructionsViewer extends Viewer<Instructions> {
+    public InstructionsViewer(Instructions model) {
+        super(model);
+    }
+
+    @Override
+    protected void drawElements(GUI gui, ImageFactory imageFactory) throws IOException {
+
+    }
 }
