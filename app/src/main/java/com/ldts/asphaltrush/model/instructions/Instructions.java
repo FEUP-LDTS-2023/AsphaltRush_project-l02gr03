49f99f4c543bc02c95decab684657c4d1afe285c
Xavier Martins <xavidosantos@gmail.com>
2023-12-13 03:57:23 +0000

    Implemented Instructions Model constructor and methods

--- app/src/main/java/com/ldts/asphaltrush/model/instructions/Instructions.java
+++ app/src/main/java/com/ldts/asphaltrush/model/instructions/Instructions.java
@@ -1,4 +1,21 @@
 package com.ldts.asphaltrush.model.instructions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Instructions {
+    private final List<String> entries;
+    private int currentEntry = 0;
+
+    public Instructions() {
+        this.entries = Arrays.asList("Back");
+    }
+
+    public boolean isSelected(int i) {
+        return currentEntry == i;
+    }
+    public boolean isSelectedBack() {
+        return isSelected(0);
+    }
 }
