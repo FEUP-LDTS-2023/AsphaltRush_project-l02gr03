939921b606017066583e3fada8968feaa914b5f8
Xavier Martins <xavidosantos@gmail.com>
2023-12-13 03:40:44 +0000

    Implemented InstructionsState constructor and methods

--- app/src/main/java/com/ldts/asphaltrush/states/InstructionsState.java
+++ app/src/main/java/com/ldts/asphaltrush/states/InstructionsState.java
@@ -1,4 +1,23 @@
 package com.ldts.asphaltrush.states;
 
-public class InstructionsState {
+import com.ldts.asphaltrush.controller.Controller;
+import com.ldts.asphaltrush.controller.instructions.InstructionsController;
+import com.ldts.asphaltrush.model.instructions.Instructions;
+import com.ldts.asphaltrush.viewer.Viewer;
+import com.ldts.asphaltrush.viewer.instructions.InstructionsViewer;
+
+public class InstructionsState extends State<Instructions>{
+    public InstructionsState(Instructions model) {
+        super(model);
+    }
+
+    @Override
+    protected Viewer<Instructions> getViewer() {
+        return new InstructionsViewer(getModel());
+    }
+
+    @Override
+    protected Controller<Instructions> getController() {
+        return new InstructionsController(getModel());
+    }
 }
