0e315f7f0875b9d2002399b8682289123c91de55
Xavier Martins <xavidosantos@gmail.com>
2023-12-13 07:42:54 +0000

    Remove unecessary code

--- app/src/main/java/com/ldts/asphaltrush/viewer/game/PlayerViewer.java
+++ app/src/main/java/com/ldts/asphaltrush/viewer/game/PlayerViewer.java
@@ -7,7 +7,6 @@ import com.ldts.asphaltrush.model.game.elements.Player;
 public class PlayerViewer implements ElementViewer<Player> {
     @Override
     public void draw(Player element, GUI gui, ImageFactory imageFactory) {
-        System.out.println(element.getType());
         gui.drawImage(element.getPosition(), imageFactory.getImage("/cars/player/game/car" + element.getType()));
     }
 }
