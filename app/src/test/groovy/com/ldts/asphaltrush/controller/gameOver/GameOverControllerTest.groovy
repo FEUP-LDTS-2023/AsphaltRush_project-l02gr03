01c82a8ab1e632170d5107bf8a711928a9183f82
brunomgferreira <brunomgferreira17@gmail.com>
2023-12-18 20:13:42 +0000

    Updated the GameOverControllerTest

--- app/src/test/groovy/com/ldts/asphaltrush/controller/gameOver/GameOverControllerTest.groovy
+++ app/src/test/groovy/com/ldts/asphaltrush/controller/gameOver/GameOverControllerTest.groovy
@@ -1,16 +1,52 @@
 package com.ldts.asphaltrush.controller.gameOver
 
-import spock.lang.Specification;
+import com.ldts.asphaltrush.Game
+import com.ldts.asphaltrush.gui.GUI
+import com.ldts.asphaltrush.model.gameOver.GameOver
+import com.ldts.asphaltrush.states.MenuState
+import spock.lang.Specification
+import spock.lang.Subject;
 
 class GameOverControllerTest extends Specification {
-    def "GameOverController Test 1"() {
-        setup:
-        def result;
+
+    @Subject
+    GameOverController gameOverController
+
+    def setup() {
+        gameOverController = new GameOverController(new GameOver(10))
+    }
+
+    def "step should move up in the model when UP action is performed"() {
+        given:
+        Game game = new Game()
 
         when:
-        result = 0;
+        gameOverController.step(game, GUI.ACTION.UP, 150)
 
         then:
-        result != 0;
+        gameOverController.getModel().isSelectedDelete()
     }
+
+    def "step should move down in the model when DOWN action is performed"() {
+        given:
+        Game game = new Game()
+
+        when:
+        gameOverController.step(game, GUI.ACTION.DOWN, 150)
+
+        then:
+        gameOverController.getModel().isSelectedBackToMenu()
+    }
+
+    def "step should change game state to MenuState when SELECT action and Back to Menu is selected"() {
+        given:
+        Game game = new Game()
+
+        when:
+        gameOverController.step(game, GUI.ACTION.SELECT, 150)
+
+        then:
+        game.getGameState().getState() instanceof MenuState
+    }
+
 }
