3b66a79d8b2d41481a1d0132bdf2d119c08614c5
brunomgferreira <brunomgferreira17@gmail.com>
2023-12-18 19:11:43 +0000

    Updated the GarageControllerTest

--- app/src/test/groovy/com/ldts/asphaltrush/controller/garage/GarageControllerTest.groovy
+++ app/src/test/groovy/com/ldts/asphaltrush/controller/garage/GarageControllerTest.groovy
@@ -1,16 +1,29 @@
 package com.ldts.asphaltrush.controller.garage
 
+import com.ldts.asphaltrush.Game
+import com.ldts.asphaltrush.gui.GUI
+import com.ldts.asphaltrush.model.garage.Garage
+import com.ldts.asphaltrush.states.GarageState
 import spock.lang.Specification;
 
 class GarageControllerTest extends Specification {
-    def "GarageController Test 1"() {
-        setup:
-        def result;
+    def "GarageController should respond correctly to user actions"() {
+        given:
+        Game game = new Game()
+        Garage garage = new Garage(0)
+        GarageState garageState = new GarageState(garage)
+        GarageController garageController = garageState.getController()
 
         when:
-        result = 0;
+        garageController.step(game, GUI.ACTION.LEFT, System.currentTimeMillis())
+        int movedLeftCar = garage.getCurrentCar()
+
+        and:
+        garageController.step(game, GUI.ACTION.RIGHT, System.currentTimeMillis())
+        int movedRightCar = garage.getCurrentCar()
 
         then:
-        result != 0;
+        movedLeftCar == 5
+        movedRightCar == 0
     }
 }
