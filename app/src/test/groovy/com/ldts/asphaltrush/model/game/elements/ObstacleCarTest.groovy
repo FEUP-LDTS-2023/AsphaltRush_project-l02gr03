837f40ba5df6ab423ffaefbe811ab42bb406890b
brunomgferreira <brunomgferreira17@gmail.com>
2023-12-18 11:25:07 +0000

    updated the ObstacleCarTest

--- app/src/test/groovy/com/ldts/asphaltrush/model/game/elements/ObstacleCarTest.groovy
+++ app/src/test/groovy/com/ldts/asphaltrush/model/game/elements/ObstacleCarTest.groovy
@@ -3,17 +3,20 @@ package com.ldts.asphaltrush.model.game.elements
 import com.ldts.asphaltrush.model.Image
 import com.ldts.asphaltrush.model.ImageFactory
 import com.ldts.asphaltrush.model.game.elements.obstacleCar.ObstacleCar
+import com.ldts.asphaltrush.model.game.elements.obstacleCar.ObstacleCarBuilder
 import spock.lang.Specification
 
 class ObstacleCarTest extends Specification {
 
+    ImageFactory imageFactory = new ImageFactory()
+    ObstacleCarBuilder obstacleCarBuilder = new ObstacleCarBuilder(new ImageFactory())
+
     def "ObstacleCar should initialize with the correct type and dimensions"() {
         given:
-        ImageFactory imageFactory = new ImageFactory()
         Image image = imageFactory.getImage("/cars/obstacle/car1")
 
         when:
-        ObstacleCar obstacleCar = new ObstacleCar(0, 0, 1)
+        ObstacleCar obstacleCar = obstacleCarBuilder.createObstacleCar(1)
 
         then:
         obstacleCar.getType() == 1
@@ -22,10 +25,10 @@ class ObstacleCarTest extends Specification {
     }
 
     def "ObstacleCar should return the correct type"() {
-        given:
-        ObstacleCar obstacleCar = new ObstacleCar(0, 0, 5)
+        when:
+        ObstacleCar obstacleCar = obstacleCarBuilder.createObstacleCar(4)
 
-        expect:
-        obstacleCar.getType() == 5
+        then:
+        obstacleCar.getType() == 4
     }
 }
