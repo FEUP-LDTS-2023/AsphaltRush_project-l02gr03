6c279ef45edc2d941e761c727ced8d2e37a117af
brunomgferreira <brunomgferreira17@gmail.com>
2023-12-18 11:40:03 +0000

    updated the ImageTest

--- app/src/test/groovy/com/ldts/asphaltrush/model/ImageTest.groovy
+++ app/src/test/groovy/com/ldts/asphaltrush/model/ImageTest.groovy
@@ -3,14 +3,44 @@ package com.ldts.asphaltrush.model
 import spock.lang.Specification;
 
 class ImageTest extends Specification {
-    def "Image Test 1"() {
-        setup:
-        def result;
+    def "Image Test: Check Width and Height"() {
+        given:
+        int width = 3
+        int height = 4
+        String[][] colorMatrix = [
+                ["R", "G", "B"],
+                ["R", "G", "B"],
+                ["R", "G", "B"],
+                ["R", "G", "B"]
+        ]
+        Image image = new Image(width, height, colorMatrix)
 
-        when:
-        result = 0;
+        expect:
+        image.width == 3
+        image.height == 4
+    }
 
-        then:
-        result != 0;
+    def "Image Test: Check Color Matrix"() {
+        given:
+        int width = 2
+        int height = 2
+        String[][] colorMatrix = [
+                ["R", "G"],
+                ["B", "Y"]
+        ]
+        Image image = new Image(width, height, colorMatrix)
+
+        expect:
+        image.colorMatrix == colorMatrix
+    }
+
+    def "Image Test: Check default values"() {
+        given:
+        Image image = new Image(0, 0, null)
+
+        expect:
+        image.width == 0
+        image.height == 0
+        image.colorMatrix == null
     }
 }
