aa1cece673c10fb108aaeb806fa7e9aac366ff30
brunomgferreira <brunomgferreira17@gmail.com>
2023-12-19 12:12:49 +0000

    Updated the RankingStateTest

--- app/src/test/groovy/com/ldts/asphaltrush/states/RankingStateTest.groovy
+++ app/src/test/groovy/com/ldts/asphaltrush/states/RankingStateTest.groovy
@@ -1,16 +1,23 @@
 package com.ldts.asphaltrush.states
 
+import com.ldts.asphaltrush.controller.ranking.RankingController
+import com.ldts.asphaltrush.model.ranking.Ranking
+import com.ldts.asphaltrush.viewer.ranking.RankingViewer
 import spock.lang.Specification;
 
 class RankingStateTest extends Specification {
-    def "RankingState Test 1"() {
-        setup:
-        def result;
+    def "RankingState should create RankingViewer and RankingController with the correct model"() {
+        given:
+        def rankingModel = new Ranking()
+        def rankingState = new RankingState(rankingModel)
 
         when:
-        result = 0;
+        def viewer = rankingState.getViewer()
+        def controller = rankingState.getController()
 
         then:
-        result != 0;
+        viewer instanceof RankingViewer
+        controller instanceof RankingController
+        controller.model == rankingModel
     }
 }
