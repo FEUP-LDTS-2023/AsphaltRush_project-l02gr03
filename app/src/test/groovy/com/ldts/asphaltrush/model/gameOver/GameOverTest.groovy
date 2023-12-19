56ec30d7ed6c0c398c674712b63baabe6dc42a2c
brunomgferreira <brunomgferreira17@gmail.com>
2023-12-19 11:33:32 +0000

    Created the GameOverTest

---
+++ app/src/test/groovy/com/ldts/asphaltrush/model/gameOver/GameOverTest.groovy
@@ -0,0 +1,82 @@
+package com.ldts.asphaltrush.model.gameOver
+
+import com.ldts.asphaltrush.model.instructions.Instructions
+import spock.lang.Specification
+
+class GameOverTest extends Specification {
+    def "moveLeft should change currentEntry correctly"() {
+        given:
+        def gameOver = new GameOver(100)
+
+        when:
+        gameOver.moveLeft()
+
+        then:
+        gameOver.currentEntry == "Confirm"
+    }
+
+    def "moveRight should change currentEntry correctly"() {
+        given:
+        def gameOver = new GameOver(100)
+
+        when:
+        gameOver.moveRight()
+
+        then:
+        gameOver.currentEntry == "Confirm"
+    }
+
+    def "moveDown should change currentEntry correctly"() {
+        given:
+        def gameOver = new GameOver(100)
+
+        when:
+        gameOver.moveDown()
+
+        then:
+        gameOver.currentEntry == "Back"
+    }
+
+    def "moveUp should change currentEntry correctly"() {
+        given:
+        def gameOver = new GameOver(100)
+        gameOver.currentEntry = 14
+
+        when:
+        gameOver.moveUp()
+
+        then:
+        gameOver.currentEntry == "F"
+    }
+
+    def "isSelectedConfirm should be true initially"() {
+        given:
+        def gameOver = new GameOver(100)
+
+        expect:
+        gameOver.isSelectedConfirm()
+    }
+
+    def "isNameEmpty should return true for an empty name"() {
+        given:
+        def gameOver = new GameOver(100)
+
+        when:
+        gameOver.setName("")
+
+        then:
+        gameOver.isNameEmpty() == true
+    }
+
+    def "isNameEmpty should return false for a non-empty name"() {
+        given:
+        def gameOver = new GameOver(100)
+
+        when:
+        gameOver.setName("John")
+
+        then:
+        gameOver.isNameEmpty() == false
+    }
+}
+
