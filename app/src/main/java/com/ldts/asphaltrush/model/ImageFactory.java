e45f0060dd775dc657fdc6dd8cf2d4f412edd2fb
Educpf <eduardocportugalf@gmail.com>
2023-12-22 00:39:53 +0000

    Commented ImageFactory

--- app/src/main/java/com/ldts/asphaltrush/model/ImageFactory.java
+++ app/src/main/java/com/ldts/asphaltrush/model/ImageFactory.java
@@ -13,19 +13,25 @@ public class ImageFactory {
         images =  new HashMap<>();
     }
 
+    // Returns the correspondent image
     public Image getImage(String image){
+        // Checks if image is already loaded
         if (!images.containsKey(image)) {
+            // Loads image from file
             Image loadedImage = loadImageFromFile(image);
+            // Saves it to use later
             images.put(image, loadedImage);
         }
         return images.get(image);
     }
 
     private Image loadImageFromFile(String image){
+        // Get the Image txt file URL
         URL resource = ImageFactory.class.getResource("/images/"+ image + ".txt");
+        // Asserts that it exists
         assert resource != null;
         try {
-
+            // Reads the .txt file and returns the correspondent image
             BufferedReader br = new BufferedReader(new FileReader(resource.getFile()));
             return readImage(br);
         } catch (IOException e){
@@ -35,17 +41,19 @@ public class ImageFactory {
     }
 
     private Image readImage(BufferedReader br) throws IOException {
+        // Gets the dimensions from the file
         int width = Integer.parseInt(br.readLine());
         int height = Integer.parseInt(br.readLine());
 
+        // The structure where the color information of the image will be stored
         String[][] colorMatrix = new String[height][width];
 
+        // Reads line by line considering the height of the image
         for (int i=0; i<height; i++){
             String line = br.readLine();
             colorMatrix[i] = line.split("-");
         }
         return new Image(width, height, colorMatrix);
-
     }
 
 }
