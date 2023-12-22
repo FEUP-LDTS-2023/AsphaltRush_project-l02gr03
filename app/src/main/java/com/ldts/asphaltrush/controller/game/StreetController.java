562b60aae2c425f1d8e2b00e509898a6a21e03f3
Educpf <eduardocportugalf@gmail.com>
2023-12-22 18:03:31 +0000

    Commented StreetController

--- app/src/main/java/com/ldts/asphaltrush/controller/game/StreetController.java
+++ app/src/main/java/com/ldts/asphaltrush/controller/game/StreetController.java
@@ -44,34 +44,47 @@ public class StreetController extends GameController {
 
     @Override
     public void step(Game game, GUI.ACTION action, long time) throws IOException, URISyntaxException, FontFormatException {
+        // Return to Menu if Quit action was pressed
         if (action == GUI.ACTION.QUIT) game.getGameState().setState(new MenuState(new Menu()));
+        // Check if player crashed
         else if (getModel().getPlayer().getCrashed()) {
+            // Play the crash sound
             crashSound.play();
+            // Change to GameOverState
             game.getGameState().setState(new GameOverState(new GameOver(getModel().getPoints().getPoints())));
         } else {
+            // If game continues call each controller individually
             playerController.step(game, action, time);
             lineController.step(game, action, time);
             obstacleCarController.step(game, action, time);
             powerUpController.step(game, action, time);
             holeController.step(game, action, time);
             pointsController.step(game, action, time);
+            // Check all the collisions between elements
             checkCollisions();
         }
     }
 
     public void checkCollisions() {
+        // Check if player collided with obstacleCar
         if (getModel().isObstacleCar(getModel().getPlayer().getPosition(), getModel().getPlayer().getWidth(), getModel().getPlayer().getHeight()) ||
                 getModel().isHole(getModel().getPlayer().getPosition(), getModel().getPlayer().getWidth(), getModel().getPlayer().getHeight())) {
+            // Get player's current PowerUp
             PowerUp playerPowerUp = getModel().getPlayer().getPowerUp();
-
+            // Ignore collision if player has Invincibility PowerUp active
             if(playerPowerUp != null && playerPowerUp.getClass() == InvenciblePowerUp.class) return;
 
+            // If not, set the player as crashed
             getModel().getPlayer().setCrashed();
         }
+        // Check if player collided with PowerUp
         if (getModel().isPowerUp(getModel().getPlayer().getPosition(), getModel().getPlayer().getWidth(), getModel().getPlayer().getHeight())) {
+            // Play the PowerUp sound
             powerUpSound.play();
+            // Remove the PowerUp from the street
             PowerUp powerUp = getModel().getPowerUp(getModel().getPlayer().getPosition(), getModel().getPlayer().getWidth(), getModel().getPlayer().getHeight());
             if(powerUp != null) getModel().getPowerUps().remove(powerUp);
+            // Add the PowerUp to player
             getModel().getPlayer().addPowerUp(powerUp);
         }
     }
